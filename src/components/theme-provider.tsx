9c51e749df2720757fc8ce8394248c4a12781c40
dheeraj080 <kambledheerajkumar@gmail.com>
2026-02-25 21:25:47 +0530

    implement: version fix

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,9 +1,12 @@
-"use client"
- 
-import * as React from "react"
-import { ThemeProvider as NextThemesProvider } from "next-themes"
-import { type ThemeProviderProps } from "next-themes/dist/types"
- 
+"use client";
+
+import * as React from "react";
+// Import both the provider and the type from the root 'next-themes'
+import {
+  ThemeProvider as NextThemesProvider,
+  type ThemeProviderProps,
+} from "next-themes";
+
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
